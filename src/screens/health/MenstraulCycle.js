20aea314f4fe17fc596265846d06df931cf8845b
yuyunfrancis <francisberi@yahoo.com>
2022-08-25 02:01:20 +0100

    booking

---
+++ src/screens/health/MenstraulCycle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MenstraulCycle = () => {
+  return (
+    <View>
+      <Text>MenstraulCycle</Text>
+    </View>
+  );
+};
+
+export default MenstraulCycle;
+
+const styles = StyleSheet.create({});
