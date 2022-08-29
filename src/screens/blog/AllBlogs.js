a21a6635e3ff7e7e0cbaee0736a07584f5d338c8
yuyunfrancis <francisberi@yahoo.com>
2022-08-29 01:41:39 +0100

    blogData

--- src/screens/blog/AllBlogs.js
+++ src/screens/blog/AllBlogs.js
@@ -1,4 +1,5 @@
 import {
+  FlatList,
   ImageBackground,
   SafeAreaView,
   ScrollView,
@@ -19,29 +20,32 @@ const blogData = [
   {
     id: 1,
     date: new Date(),
+    author: "Francis",
     image: images.doc1,
     title: "Breast Cancer and its effect to humanity",
     details:
       "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout, the final text replacing the false text as soon as it is ready or the layout is completed. Generally, a false Latin text, Lorem ipsum or Lipsum, is used.",
-    likes: "25",
+    likes: 25,
   },
   {
     id: 2,
     date: new Date(),
+    author: "Francis",
     image: images.doc3,
     title: "Breast Cancer and its effect to humanity",
     details:
       "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout, the final text replacing the false text as soon as it is ready or the layout is completed. Generally, a false Latin text, Lorem ipsum or Lipsum, is used.",
-    likes: "20",
+    likes: 20,
   },
   {
     id: 3,
     date: new Date(),
+    author: "Francis",
     image: images.doc2,
     title: "Breast Cancer and its effect to humanity",
     details:
       "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout, the final text replacing the false text as soon as it is ready or the layout is completed. Generally, a false Latin text, Lorem ipsum or Lipsum, is used.",
-    likes: "50",
+    likes: 0,
   },
 ];
 
@@ -49,6 +53,87 @@ const AllBlogs = () => {
   const navigation = useNavigation();
   const [searchQuery, setSearchQuery] = useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigation.navigate("BlogDetails", item)}
+        key={item.id}
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginBottom: 25,
+        }}
+      >
+        <View>
+          <ImageBackground
+            imageStyle={{ borderRadius: 8 }}
+            resizeMode="cover"
+            source={item.image}
+            style={{
+              width: SIZES.screenWidth * 0.4,
+              height: SIZES.screenWidth * 0.3,
+            }}
+          ></ImageBackground>
+        </View>
+        <View style={{ paddingLeft: 20, marginRight: 14 }}>
+          <Text
+            style={{
+              color: "gray",
+              fontFamily: "Poppins_Regular",
+              fontSize: 12,
+              marginBottom: 8,
+              width: SIZES.screenWidth * 0.4,
+            }}
+          >
+            {item.date.toString("")}
+          </Text>
+          <Text
+            numberOfLines={3}
+            style={{
+              width: SIZES.screenWidth * 0.51,
+              paddingRight: 5,
+              fontFamily: "Poppins_SemiBold",
+              color: COLORS.textColor,
+              fontSize: 15.5,
+            }}
+          >
+            {item.title}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 12,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginRight: 15,
+              }}
+            >
+              <TouchableOpacity>
+                <Icon name="ios-heart-outline" size={24} />
+              </TouchableOpacity>
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontFamily: "Poppins_Regular",
+                  marginLeft: 8,
+                  color: "gray",
+                }}
+              >
+                {`${item.likes} ${item.likes > 0 ? "likes" : "like"}`}
+              </Text>
+            </View>
+            <Icon name="ios-share-outline" size={24} />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <>
@@ -76,7 +161,7 @@ const AllBlogs = () => {
               iconColor="#D2D1D1"
             />
           </View>
-          {blogData.map((data) => {
+          {/* {blogData.map((data) => {
             return (
               <TouchableOpacity
                 onPress={() => navigation.navigate("BlogDetails", data)}
@@ -147,7 +232,7 @@ const AllBlogs = () => {
                           color: "gray",
                         }}
                       >
-                        {data.likes} likes
+                        {`${data.likes} ${data.likes > 0 ? "likes" : "like"}`}
                       </Text>
                     </View>
                     <Icon name="ios-share-outline" size={24} />
@@ -155,7 +240,12 @@ const AllBlogs = () => {
                 </View>
               </TouchableOpacity>
             );
-          })}
+          })} */}
+          <FlatList
+            data={blogData}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
         </ScrollView>
       </SafeAreaView>
     </>
--- src/screens/blog/BlogDetails.js
+++ src/screens/blog/BlogDetails.js
@@ -4,17 +4,28 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+
 import { AppStatusBar, CustomStatusBar } from "../../components";
 import { COLORS, SIZES } from "../../utility";
 
 const BlogDetails = ({ route }) => {
+  const [like, setLike] = useState(false);
+
   const item = route.params;
   console.log("====================================");
   console.log("Items", item);
   console.log("====================================");
+
+  const onLike = () => {
+    setLike(!like);
+  };
+
   return (
     <>
       <AppStatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
@@ -28,16 +39,76 @@ const BlogDetails = ({ route }) => {
             marginHorizontal: 15,
           }}
         >
-          <View style={{ alignItems: "center" }}>
+          <View style={{ alignItems: "center", marginBottom: 10 }}>
             <Image
               resizeMode="cover"
               source={item.image}
               style={{
                 width: SIZES.screenWidth * 0.9,
-                height: SIZES.screenHeight * 0.25,
+                height: SIZES.screenHeight * 0.22,
                 borderRadius: 12,
               }}
             />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginHorizontal: 10,
+              marginTop: 8,
+            }}
+          >
+            <Text style={{ fontFamily: "Poppins_Medium", fontSize: 15 }}>
+              <Text style={{ color: "gray" }}>By</Text>
+              {` ${item.author}`}
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  marginRight: 15,
+                }}
+              >
+                <TouchableOpacity onPress={onLike} style={{ marginRight: 2 }}>
+                  <Icon
+                    name={like ? "ios-heart" : "heart-outline"}
+                    size={24}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+                <Text>{`${item.likes} ${
+                  item.likes > 1 ? "likes" : "like"
+                }`}</Text>
+              </View>
+              <TouchableOpacity>
+                <Icon name="ios-share-outline" size={24} color="#333333" />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View
+            style={{
+              paddingHorizontal: 10,
+              paddingTop: SIZES.screenHeight * 0.03,
+            }}
+          >
+            <Text
+              style={{
+                lineHeight: 23,
+                color: "#5A5858",
+                fontFamily: "Poppins_Regular",
+                fontSize: 14,
+              }}
+            >
+              {item.details}
+            </Text>
           </View>
         </ScrollView>
       </SafeAreaView>
